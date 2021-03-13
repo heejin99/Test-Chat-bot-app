271643d5d71a21ba74a6e22410c2f7584e8ded46
heejin99 <wkdgmlwls30@naver.com>
2021-03-13 21:59:28 +0900

    챗봇 수정
    - inflearn 강의

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -4,24 +4,22 @@ const structjson = require('./structjson.js');
 const dialogflow = require('dialogflow');
 const uuid = require('uuid');
 
-const config = require('../config/keys');
+const config = require('../config/keys')
 
 const projectId = config.googleProjectID
 const sessionId = config.dialogFlowSessionID
-const languageCode = config.dialogFlowSessionLanguageCode
-
-
+const languageCode = config.languageCode
 // Create a new session
 const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-// We will make two routes 
-
+// We will make two routes
 
 // Text Query Route
 
 router.post('/textQuery', async (req, res) => {
-    //We need to send some information that comes from the client to Dialogflow API 
+    // We need to send some info that comes from the client to Dialogflow Api
+
     // The text query request.
     const request = {
         session: sessionPath,
@@ -34,7 +32,7 @@ router.post('/textQuery', async (req, res) => {
             },
         },
     };
-
+    
     // Send request and log result
     const responses = await sessionClient.detectIntent(request);
     console.log('Detected intent');
@@ -46,38 +44,5 @@ router.post('/textQuery', async (req, res) => {
 })
 
 
-
-//Event Query Route
-
-router.post('/eventQuery', async (req, res) => {
-    //We need to send some information that comes from the client to Dialogflow API 
-    // The text query request.
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            event: {
-                // The query to send to the dialogflow agent
-                name: req.body.event,
-                // The language used by the client (en-US)
-                languageCode: languageCode,
-            },
-        },
-    };
-
-    // Send request and log result
-    const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-
-    res.send(result)
-})
-
-
-
-
-
-
-
+// Event Query Route
 module.exports = router;
