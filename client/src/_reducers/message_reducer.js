db81fa03f012136a148daa8772e5e14226414f5d
heejin99 <wkdgmlwls30@naver.com>
2021-03-15 21:50:13 +0900

    챗봇 수정

--- client/src/_reducers/message_reducer.js
+++ client/src/_reducers/message_reducer.js
@@ -1,13 +1,13 @@
 import {
-    SAMPLE,
+    SAVE_MESSAGE,
 } from '../_actions/types';
 
 export default function (state = {messages:[]}, action) {
     switch (action.type) {
-        case SAMPLE:
+        case SAVE_MESSAGE:
             return {
                 ...state,
-                messages: state
+                messages: state.messages.concat(action.payload) // payload: 데이터
             }
         default:
             return state;
